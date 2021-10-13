5a9b1f08e404943044cea2dfbe77b5aa93a6fe03
Christy Jacob <christyjacob4@gmail.com>
2021-10-14 01:55:42 +0530

    feat: updated loading animation

--- pages/index.vue
+++ pages/index.vue
@@ -79,7 +79,7 @@
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
+          class="h-5 w-5 text-white"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -95,7 +95,7 @@
 
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6 animate-spin"
+          class="h-5 w-5 animate-spin text-white"
           v-else-if="loading"
           fill="none"
           viewBox="0 0 24 24"
